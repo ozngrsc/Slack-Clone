87675d2562d85a364b276565ddb062be0cbb20ed
ozngrsc <ozangursucu@hotmail.com>
2022-06-22 11:51:29 +0300

    message style

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -6,7 +6,12 @@ function Message({ message, timestamp, user, userImage }) {
     <div className="message">
       <img src={userImage} alt="" />
       <div className="message__info">
-        <h4>{user} timestamp...</h4>
+        <h4>
+          {user}{" "}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
+        </h4>
         <p>{message}</p>
       </div>
     </div>
